27c4b06cfb4247bfee80e677db4eb30f2cecf375
Ben Swift <ben@benswift.me>
2019-07-22 08:36:34 +1000

    add template code for making the nametag

--- sketch.js
+++ sketch.js
@@ -1,8 +1,28 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  // put setup code here
+  // create the canvas (800px wide, 600px high)
+  createCanvas(800, 600);
+
+  // make the text nice and big - adjust the size parameter
+  // to make *your* name fit nicely on the nametag
+  textSize(150);
+
+  // draw a border to help you see the size
+  // this isn't compulsory (remove this code if you like)
+  strokeWeight(5);
+  rect(0, 0, width, height);
 }
 
 function draw() {
-  // put drawing code here
+  // your cool nametag code goes in this draw function
+
+  // replace my name with your name!
+  text("Ben", 100, height-100);
+}
+
+// when you hit the spacebar, what's currently on the canvas will be saved (as a
+// "nametag.png" file) to your downloads folder
+function keyTyped() {
+  if (key === " ") {
+	saveCanvas("nametag.png");
+  }
 }
